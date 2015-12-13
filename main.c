9b4f665143bbfda93a7ab6d4090e49dce634738c
Gabriel Toublanc <gabriel@phoxygen.com>
2015-12-13 23:24:37 +0100

    Remove skiping algorithm and fix KMP that was improperly coded

--- main.c
+++ main.c
@@ -3,6 +3,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+** The program try to match PATTERN into TEXT but with PATTERN being
+** a p-string, a parametrized string where each letter in the pattern
+** can match even if the letter in the TEXT as long as every occurence
+** of this letter in the PATTERN is replaced by the same letter each time.
+*/
+
 /* preCompute a string 'str' into an array of distance 'render' */
 int *preCompute(char *str, int len) {
   int *render;
@@ -23,10 +30,11 @@ int *preCompute(char *str, int len) {
   return render;
 }
 
-/* xP and yP are array preComputed, m and n are the length of xP and yP */
+/* Naive algorithm for p-strings */
 void naiveAlgorithm(int *xP, int *yP, int m, int n) {
   int i, j;
   int c = 0;
+  int matchNb = 0;
 
   printf("matchs: [");
   for (j = 0; j < n - m + 1; ++j) {
@@ -35,33 +43,18 @@ void naiveAlgorithm(int *xP, int *yP, int m, int n) {
       if (!(xP[i] == yP[j+i] || (!xP[i] && yP[j+i] >= m - i)))
 	break;
     }
-    if (i == m)
-      printf("%d, ", j);
-  }
-  printf("]\nNaive algorithm: %d comparisons\n\n", c);
-}
-
-/* same as above + skips characters when there's a mismatch */
-void skipAlgorithm(int *xP, int *yP, int m, int n) {
-  int i, j;
-  int c = 0;
-
-  printf("matchs: [");
-  for (j = 0; j < n - m + 1; ++j) {
-    for (i = 0; i < m && j < n - m + 1; ++i) {
-      ++c;
-      if (!(xP[i] == yP[j+i] || (!xP[i] && yP[j+i] >= m - i)))
-	break;
+    if (i == m) {
+      if (matchNb)
+	printf(", ");
+      printf("%d", j);
+      ++matchNb;
     }
-    if (i == m)
-      printf("%d, ", j);
-    else if (xP[0]) /* if not, KMP */
-      j += i;
   }
-  printf("]\nSkiping algorithm: %d comparisons\n\n", c);
+  printf("]\nNaive algorithm: %d(search) + %d(preCompute) == %d\n\n", c, m+n, c+m+n);
 }
 
-int *preKMP(char *x, int m) {
+/* KMP precompute array of next prefix */
+int *preKMP(int *x, int m) {
   int i, idx;
   int *T;
 
@@ -77,11 +70,12 @@ int *preKMP(char *x, int m) {
   return T;
 }
 
-/* KMP algorithm */
-void KMPAlgorithm(char *x, int *xP, int *yP, int m, int n) {
+/* KMP algorithm for p-strings */
+void KMPAlgorithm(int *xP, int *yP, int m, int n) {
   int i = 0, j;
-  int *T = preKMP(x, m);
+  int *T = preKMP(xP, m);
   int c = 0;
+  int matchNb = 0;
 
   printf("matchs: [");
   for (j = 0; j < n - m + 1; ++j) {
@@ -90,37 +84,17 @@ void KMPAlgorithm(char *x, int *xP, int *yP, int m, int n) {
       if (!(xP[i] == yP[j+i] || (!xP[i] && yP[j+i] >= m - i)))
 	break;
     }
-    if (i == m)
-      printf("%d, ", j);
+    if (i == m) {
+      if (matchNb)
+	printf(", ");
+      printf("%d", j);
+      ++matchNb;
+    }
     else if (i)
       j += i - T[i - 1] - 1;
     i = (i == m || !i ? 0 : T[i - 1]);
   }
-  printf("]\nKMP algorithm: %d comparisons + %d from construct == %d\n\n", c, m, c+m);
-}
-
-/* skip + KMP algorithm */
-void skipKMPAlgorithm(char *x, int *xP, int *yP, int m, int n) {
-  int i = 0, j;
-  int *T = preKMP(x, m);
-  int c = 0;
-
-  printf("matchs: [");
-  for (j = 0; j < n - m + 1; ++j) {
-    for (; i < m && j < n - m + 1; ++i) {
-      ++c;
-      if (!(xP[i] == yP[j+i] || (!xP[i] && yP[j+i] >= m - i)))
-	break;
-    }
-    if (i == m)
-      printf("%d, ", j);
-    else if (xP[0])
-      j += i;
-    else if (i)
-      j += i - T[i - 1] - 1;
-    i = (i == m || xP[0] || !i ? 0 : T[i - 1]);
-  }
-  printf("]\nSkipingKMP algorithm: %d comparisons + %d from construct == %d\n\n", c, m, c+m);
+  printf("]\nKMP algorithm: %d(search) + %d(preCompute) == %d\n\n", c, m+m+n, c+m+m+n);
 }
 
 int main(int ac, char **av) {
@@ -135,9 +109,7 @@ int main(int ac, char **av) {
     xP = preCompute(av[1], m);
     yP = preCompute(av[2], n);
     naiveAlgorithm(xP, yP, m, n);
-    KMPAlgorithm(av[1], xP, yP, m, n);
-    skipAlgorithm(xP, yP, m, n);
-    skipKMPAlgorithm(av[1], xP, yP, m, n);
+    KMPAlgorithm(xP, yP, m, n);
   }
   return 0;
 }
